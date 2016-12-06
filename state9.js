a222b6b52c12cfb78d51228f504c93635eab23c9
U-martin_nemecek\Admin <mothlggray@protonmail.com>
2016-12-06 21:34:21 +0100

    implemented firebase functionality

--- state9.js
+++ state9.js
@@ -1,10 +1,18 @@
-var highscoreText = [], highscore = [10,9,8,7,6,5,4,3,2,1];
+var highscoreText = [], highscore = [10,9,8,7,6,5,4,3,2,1], firebaseRef, snapval;
 
 demo.state9 = function(){};
 demo.state9.prototype = {
-    preload: function(){},
+    preload: function(){
+        game.load.image("button1", "art/bank_job_button.png");
+        game.load.image("button2", "art/gun.png");
+    },
     create: function(){
         addAllChangeStateEventListeners();
+        //special firebase link to our database as a parameter
+        //firebaseRef = new Firebase("https://phase-demo-game.firebaseio.com/");
+        
+        //this is not only for convenience as it seems, it is somehow essential, maybe not or maybe less than snapval
+        firebaseRef = firebase.database().ref();
         
         //the ranking numbers
         for (var i = 1; i<11; i++) {
@@ -16,6 +24,40 @@ demo.state9.prototype = {
             //what is highscoreText good for anyway
             highscoreText[i] = game.add.text(300, (i+1)*55, highscore[i], {fontSize: "35px"});
         }
+        
+        //his trick, don't understand
+        var updateHighscore = this.updateHighscore;
+        //snapshot is a firebase terminus technicus 
+        
+        
+        firebaseRef.on("value", function(snapshot) {
+            //this is very sensitive, we need to assign snapshot.val() to a variable here
+            snapval = snapshot.val();
+            updateHighscore(snapval.highscore);
+        });
+        
+        //buttons
+        game.add.button(800, 400, "button1", function() {
+            var randomScore = Math.round(Math.random()*100);
+            //why am i using push
+            snapval.highscore.push(randomScore);
+            //sorting and cutting first ten
+            snapval.highscore = snapval.highscore.sort( function(a,b) {
+                //for some reason b-a is from the greatest to lowest and a-b is the opposite
+                return b-a;
+                //slice includes the first parameter, but excludes the second one
+            }).slice(0, 10);
+            firebaseRef.set(snapval);
+        });
+        game.add.button(800, 550, "button2", function() {
+            firebaseRef.set({highscore: [0,0,0,0,0,0,0,0,0,0]});
+        });
     },
-    update: function(){}
+    update: function(){},
+    
+    updateHighscore: function(highscore) {
+        for (var i = 0; i<10; i++) {
+            highscoreText[i].text = highscore[i];
+        }
+    }
 };
